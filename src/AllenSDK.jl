6ea8ba54efbbaf1d3aa6e4b81fe65623707496db
brendanjohnharris <brendanjohnharris@gmail.com>
2023-08-09 11:31:41 +1000

    Init

--- src/AllenSDK.jl
+++ src/AllenSDK.jl
@@ -1,5 +1,16 @@
 module AllenSDK
+using PythonCall
 
-# Write your package code here.
+export pynwb, allensdk, behavior_ecephys_session
+
+const pynwb = PythonCall.pynew()
+const allensdk = PythonCall.pynew()
+const behavior_ecephys_session = PythonCall.pynew()
+
+function __init__()
+    PythonCall.pycopy!(pynwb, pyimport("pynwb"))
+    PythonCall.pycopy!(allensdk, pyimport("allensdk"))
+    PythonCall.pycopy!(behavior_ecephys_session, pyimport("allensdk.brain_observatory.ecephys.behavior_ecephys_session"))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using AllenSDK
 using Test
 
 @testset "AllenSDK.jl" begin
-    # Write your tests here.
+    # Load the test file
+    test_file = joinpath(dirname(pathof(AllenSDK)), "..", "test", "ecephys_session_1152811536.nwb")
+    @test isfile(test_file)
+    @test_nowarn f = behavior_ecephys_session.BehaviorSession.from_nwb_path(test_file)
 end
