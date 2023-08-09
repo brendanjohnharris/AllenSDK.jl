ead48244b3c5f2bf091df25426078395871f991c
brendanjohnharris <brendanjohnharris@gmail.com>
2023-08-09 16:55:57 +1000

    Up deps

--- src/AllenSDK.jl
+++ src/AllenSDK.jl
@@ -1,16 +1,21 @@
 module AllenSDK
 using PythonCall
 
-export pynwb, allensdk, behavior_ecephys_session
+export pynwb, allensdk, behavior_ecephys_session, ecephys
 
 const pynwb = PythonCall.pynew()
 const allensdk = PythonCall.pynew()
 const behavior_ecephys_session = PythonCall.pynew()
+const ecephys = PythonCall.pynew()
+const ecephys_project_cache = PythonCall.pynew()
+
 
 function __init__()
     PythonCall.pycopy!(pynwb, pyimport("pynwb"))
     PythonCall.pycopy!(allensdk, pyimport("allensdk"))
     PythonCall.pycopy!(behavior_ecephys_session, pyimport("allensdk.brain_observatory.ecephys.behavior_ecephys_session"))
+    PythonCall.pycopy!(ecephys, pyimport("allensdk.brain_observatory.ecephys"))
+    PythonCall.pycopy!(ecephys_project_cache, pyimport("allensdk.brain_observatory.ecephys.ecephys_project_cache"))
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,6 @@ using Test
     # Load the test file
     test_file = joinpath(dirname(pathof(AllenSDK)), "..", "test", "ecephys_session_1152811536.nwb")
     @test isfile(test_file)
-    @test_nowarn f = behavior_ecephys_session.BehaviorSession.from_nwb_path(test_file)
+    @test_nowarn m = ecephys._behavior_ecephys_metadata.BehaviorEcephysMetadata.from_nwb(test_file)
+    @test_nowarn f = behavior_ecephys_session.BehaviorEcephysSession.from_nwb_path(test_file)
 end
