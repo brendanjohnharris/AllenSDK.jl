e8a37413a507ed29a9ef9e0068bc524742451806
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2024-01-17 15:42:53 +1100

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,24 +3,21 @@ using PythonCall
 using Test
 
 @testset "AllenSDK.jl" begin
-    logging = pyimport("logging")
-    logging.getLogger().setLevel(logging.DEBUG)
     # Load the test file
     ecephyscache = @test_nowarn pyimport("allensdk.brain_observatory.ecephys.ecephys_project_cache")
+    behaviorcache = @test_nowarn pyimport("allensdk.brain_observatory.behavior.behavior_project_cache")
     manifestdir = joinpath(dirname(pathof(AllenSDK)), "..", "test", "testdata")
-    manifest = joinpath(manifestdir, "manifest.json")
-    cache = @test_nowarn ecephyscache.EcephysProjectCache(; manifest)
-    @test isfile(manifest)
+    cache = behaviorcache.VisualBehaviorNeuropixelsProjectCache.from_s3_cache(cache_dir=manifestdir)
 
-    sessions = @test_nowarn cache.get_session_table()
-    probes = @test_nowarn cache.get_probes()
-    channels = @test_nowarn cache.get_channels()
+    @test isfile(joinpath(manifestdir, pyconvert(String, cache.latest_manifest_file())))
 
+    sessions = @test_nowarn cache.get_ecephys_session_table()
+    probes = @test_nowarn cache.get_probe_table()
+    channels = @test_nowarn cache.get_channel_table()
+    units = @test_nowarn cache.get_unit_table()
 
-    @test isfile(joinpath(manifestdir, "probes.csv"))
-    @test isfile(joinpath(manifestdir, "channels.csv"))
-    @test isfile(joinpath(manifestdir, "sessions.csv"))
-
+    session = pyconvert(Int, sessions.index[1])
+    session = cache.get_ecephys_session(session)
 
     # f = behavior_ecephys_session.BehaviorEcephysSession.from_nwb_path(test_file)
     # @test f isa PythonCall.Py
