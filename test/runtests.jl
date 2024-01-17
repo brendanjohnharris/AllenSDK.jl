caeb86e5d5d6c0365e80b7e1c6c0c7f8a9bcde83
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2024-01-17 15:21:16 +1100

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,28 @@
 using AllenSDK
+using PythonCall
 using Test
 
 @testset "AllenSDK.jl" begin
+    logging = pyimport("logging")
+    logging.getLogger().setLevel(logging.DEBUG)
     # Load the test file
-    test_file = joinpath(dirname(pathof(AllenSDK)), "..", "test", "ecephys_session_1152811536.nwb")
-    @test isfile(test_file)
-    @test_nowarn m = ecephys._behavior_ecephys_metadata.BehaviorEcephysMetadata.from_nwb(test_file)
-    @test_nowarn f = behavior_ecephys_session.BehaviorEcephysSession.from_nwb_path(test_file)
+    ecephyscache = @test_nowarn pyimport("allensdk.brain_observatory.ecephys.ecephys_project_cache")
+    manifestdir = joinpath(dirname(pathof(AllenSDK)), "..", "test", "testdata")
+    manifest = joinpath(manifestdir, "manifest.json")
+    cache = @test_nowarn ecephyscache.EcephysProjectCache(; manifest)
+    @test isfile(manifest)
+
+    sessions = @test_nowarn cache.get_session_table()
+    probes = @test_nowarn cache.get_probes()
+    channels = @test_nowarn cache.get_channels()
+
+
+    @test isfile(joinpath(manifestdir, "probes.csv"))
+    @test isfile(joinpath(manifestdir, "channels.csv"))
+    @test isfile(joinpath(manifestdir, "sessions.csv"))
+
+
+    # f = behavior_ecephys_session.BehaviorEcephysSession.from_nwb_path(test_file)
+    # @test f isa PythonCall.Py
+    # @test pyconvert(Array, f.spike_times) isa Vector{Int}
 end
