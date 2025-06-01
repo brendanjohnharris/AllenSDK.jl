407e3ccaf1366bd37d473db29edd293bd0309dc4
Brendan Harris <brendanjohnharris@gmail.com>
2025-06-02 00:41:09 +1000

    Fix for windows by using pip?

--- src/AllenSDK.jl
+++ src/AllenSDK.jl
@@ -8,14 +8,15 @@ const allensdk = PythonCall.pynew()
 const behavior_ecephys_session = PythonCall.pynew()
 const ecephys = PythonCall.pynew()
 const ecephys_project_cache = PythonCall.pynew()
-
+const h5py = PythonCall.pynew()
 
 function __init__()
     PythonCall.pycopy!(pynwb, pyimport("pynwb"))
     PythonCall.pycopy!(allensdk, pyimport("allensdk"))
     PythonCall.pycopy!(behavior_ecephys_session, pyimport("allensdk.brain_observatory.ecephys.behavior_ecephys_session"))
     PythonCall.pycopy!(ecephys, pyimport("allensdk.brain_observatory.ecephys"))
-    PythonCall.pycopy!(ecephys_project_cache, pyimport("allensdk.brain_observatory.ecephys.ecephys_project_cache"))
+    PythonCall.pycopy!(ecephys_project_cache,
+        pyimport("allensdk.brain_observatory.ecephys.ecephys_project_cache"))
 end
 
 end
