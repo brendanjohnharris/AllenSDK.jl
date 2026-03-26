ca28ba2f608f527cfef2070f535650497ee8f071
brendanjohnharris <brendanjohnharris@gmail.com>
2026-03-26 20:35:11 +1100

    Try fix again?

--- src/AllenSDK.jl
+++ src/AllenSDK.jl
@@ -1,5 +1,6 @@
 module AllenSDK
 using PythonCall
+import CondaPkg
 
 export pynwb, allensdk, behavior_ecephys_session, ecephys
 
@@ -11,6 +12,13 @@ const ecephys_project_cache = PythonCall.pynew()
 const h5py = PythonCall.pynew()
 
 function __init__()
+    # On Windows, add the conda env's Library/bin to PATH so h5py can find the HDF5 DLLs
+    if Sys.iswindows()
+        libbin = joinpath(CondaPkg.envdir(), "Library", "bin")
+        if isdir(libbin)
+            ENV["PATH"] = libbin * ";" * get(ENV, "PATH", "")
+        end
+    end
     PythonCall.pycopy!(pynwb, pyimport("pynwb"))
     PythonCall.pycopy!(allensdk, pyimport("allensdk"))
     PythonCall.pycopy!(behavior_ecephys_session, pyimport("allensdk.brain_observatory.ecephys.behavior_ecephys_session"))
